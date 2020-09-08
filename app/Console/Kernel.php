2e5c5c49fba2f7210e925a8b722b728b5425bd20
Bashir Shallah <bashir.shallah@gmail.com>
2020-09-08 18:21:41 +0300

    backup to aws s3 lesson.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,6 +25,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->command('backup:clean')->onOneServer()->daily()->at('01:00');
+        $schedule->command('backup:run')->onOneServer()->everyFifteenMinutes();
     }
 
     /**
